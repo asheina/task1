f4fd693a35cc1199ba595e50ac17485d08f0443b
Arina Sheina <asheina@ozon.ru>
2021-10-11 22:14:31 +0300

    add DataItem full

--- DataItem.cs
+++ DataItem.cs
@@ -5,7 +5,6 @@ public struct DataItem
     public double x { get; set; }
     public double y { get; set; }
     public System.Numerics.Complex value { get; set; }
-
     public DataItem(double x, double y, System.Numerics.Complex val)
     {
         this.x = x;
@@ -14,11 +13,17 @@ public struct DataItem
     }
     public string ToLongString(string format)
     {
-        return "";
+        string res = "(x, y) : (" + String.Format(format, x) + ", " +
+                String.Format(format, y) +
+                ")\n";
+        res += "value = " + value.ToString() +
+                " |value| = " + System.Numerics.Complex.Abs(value) +
+                "\n";
+        return res;
     }
 
     public override string ToString()
     {
-        return "";
+        return value.ToString();
     }
 }
